8ee7d6bd45a9a29452e9c4ed7114812c93c276b6
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-13 01:45:46 +0200

    get services

---
+++ get_services.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include('connection.php');
+$patients= $mysqli->prepare('select s.id, u.name as patient, u2.name as employee, s.description, s.cost, d.name, s.approved from services s join users u on s.patient_id = u.id join users u2 on s.employee_id = u2.id join departments d on s.department_id = d.id');
+$patients->execute();
+$array = $patients->get_result();
+
+$response = [];
+while ($a = $array->fetch_assoc()) {
+    $response[] = $a;
+}
+echo json_encode($response);
+
+?>
